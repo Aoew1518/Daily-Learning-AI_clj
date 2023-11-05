6f4ed5a75b4e502654a26c231ff7db6c418f9664
Aoew1518 <1518703146@qq.com>
2023-11-05 23:35:45 +0800

    小程序

--- wxapp/dreamcar/app.js
+++ wxapp/dreamcar/app.js
@@ -1,15 +1,33 @@
+// // app.js
+// App({
+//   onLaunch() {
+//     // 展示本地存储能力
+//     const logs = wx.getStorageSync('logs') || []
+//     logs.unshift(Date.now())
+//     wx.setStorageSync('logs', logs)
+
+//     // 登录
+//     wx.login({
+//       success: res => {
+//         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+//       }
+//     })
+//   },
+//   globalData: {
+//     userInfo: null
+//   }
+// })
+
 // app.js
 App({
+  // 生命周期函数
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    console.log('应用启动了')
+    wx.request({
+      url: 'https://resources.ninghao.net/wxapp-case/db.json',
+      success: (response) => {
+        // console.log(response)
+        this.globalData = response.data
       }
     })
   },
