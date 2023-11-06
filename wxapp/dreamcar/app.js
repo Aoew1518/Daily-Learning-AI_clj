5bece2230c76ba0662b074892d8713ab3b91a3d9
Aoew1518 <1518703146@qq.com>
2023-11-06 16:45:01 +0800

    微信小程序

--- wxapp/dreamcar/app.js
+++ wxapp/dreamcar/app.js
@@ -1,37 +1,14 @@
-// // app.js
-// App({
-//   onLaunch() {
-//     // 展示本地存储能力
-//     const logs = wx.getStorageSync('logs') || []
-//     logs.unshift(Date.now())
-//     wx.setStorageSync('logs', logs)
-
-//     // 登录
-//     wx.login({
-//       success: res => {
-//         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-//       }
-//     })
-//   },
-//   globalData: {
-//     userInfo: null
-//   }
-// })
-
 // app.js
 App({
-  // 生命周期函数
+    // 生命周期函数
   onLaunch() {
-    console.log('应用启动了')
+    console.log('应用启动了');
     wx.request({
       url: 'https://resources.ninghao.net/wxapp-case/db.json',
-      success: (response) => {
-        // console.log(response)
-        this.globalData = response.data
-      }
+      success:(response)=>{
+        this.globalData=response.data;
+    }
     })
   },
-  globalData: {
-    userInfo: null
-  }
+  
 })
--- wxapp/dreamcar/pages/vehicles/show.js
+++ wxapp/dreamcar/pages/vehicles/show.js
@@ -1,66 +1,88 @@
 // pages/vehicles/show.js
+const app=getApp();
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+      design:[
+      //   {
+      //   id:1,
+      //   header: '闻名遐迩的设计',
+      //   image: 'https://resources.ninghao.net/landrover/range-rover-small.jpg',
+      //   description: '奢华奢华奢华奢华奢华奢华奢华'
+      // },{
+      //   id:2,
+      //   header: '闻名遐迩的设计',
+      //   image: 'https://resources.ninghao.net/landrover/range-rover-small.jpg',
+      //   description: '奢华奢华奢华奢华奢华奢华奢华'
+      // }
+    ]
+    },
+    testDrive(){
+        wx.showToast({
+          title: '我们的工作人员会和您联系',
+        })
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    //api data 数据修改，自动更新页面 MVVM
+    onLoad(options) {
+      // console.log(app.globalData);
+        this.setData({
+            design:
+            app.globalData.vehicles[0].meta.exterior_design
+        })
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
+    },
 
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
+    },
 
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
+    },
 
-  },
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+    }
 })
