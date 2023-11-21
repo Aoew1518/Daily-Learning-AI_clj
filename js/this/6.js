d9c06607e4daa02912017c0901f596d89c20e4dd
Aoew1518 <1518703146@qq.com>
2023-11-21 17:02:15 +0800

    js

--- js/this/6.js
+++ js/this/6.js
@@ -1,5 +1,13 @@
-var Foo = () => {
-}
-new Foo();
+// var Foo = () => {
+// }
+// new Foo();
 // 箭头函数不能做构造函数
 //输出会报错
+
+function greet(name) {
+    console.log(`Hello, ${name}! My name is ${this.name}.`);
+  }
+  
+  const person = { name: 'John' };
+  
+  greet.call(person, 'Alice');
