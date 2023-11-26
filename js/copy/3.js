fa0120fb38d90008690b28eaa15c8b6af9e647ff
Aoew1518 <1518703146@qq.com>
2023-11-26 23:35:32 +0800

    js

---
+++ js/copy/3.js
@@ -0,0 +1,21 @@
+let obj = {
+  name: '李总',
+  age: 18,
+  a: {
+    n: 1
+  },
+  b: undefined,
+  c: null,
+  d: function() {},
+  e: Symbol('hello'),
+  f: {
+    n: 100
+  }
+}
+obj.e = obj.f
+obj.f.n = obj.e
+
+// console.log(obj);
+
+let obj2 = JSON.parse(JSON.stringify(obj))
+console.log(obj2);
