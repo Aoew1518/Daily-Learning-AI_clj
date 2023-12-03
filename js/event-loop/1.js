3486840f2a57b3c9477940792511d936149c231f
Aoew1518 <1518703146@qq.com>
2023-12-03 18:40:51 +0800

    js

---
+++ js/event-loop/1.js
@@ -0,0 +1,17 @@
+setTimeout(() => {
+  console.log(1);
+}, 1000)
+
+console.log(2);
+
+for (let i = 0; i < 100000; i++) {
+
+}
+
+
+
+setImmediate(() => {
+  // xxxxx
+})
+
+// xxxxx
---
+++ js/event-loop/3.js
@@ -0,0 +1,25 @@
+console.log('stard');   
+async function async1() {
+  await async2()  // 浏览器给await开小灶提速
+  console.log('saync1 end');  // 被await挤入微任务队列
+} 
+async function async2() {
+  console.log('saync2 end');
+}
+async1()
+setTimeout(function() {
+  console.log('setTimeout');
+}, 0)
+new Promise((resolve) => {
+  console.log('promise');
+  resolve()
+})
+.then(() => {
+  console.log('then1');
+})
+.then(() => {
+  console.log('then2');
+})
+console.log('end');
+
+// stard   saync2end   promise   end    saync1end   then1  then2  setTimeout
