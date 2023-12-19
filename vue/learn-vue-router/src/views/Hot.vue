659b4a60449cf5776450588bc64ca1b2c3fb4a8b
Aoew1518 <1518703146@qq.com>
2023-12-19 11:57:44 +0800

    vue

---
+++ vue/learn-vue-router/src/views/Hot.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>沸点页面</h2>
+
+    <button @click="goClassPage">去课程页面</button>
+      
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    goClassPage() {
+      // this.$router.push('/class')
+      // this.$router.push({path: '/class', query: {id: 123, name: 'Tom' }})
+      this.$router.push({ name: 'class', params: { id: 456, name: 'Tom'}})
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
