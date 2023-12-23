6c4be904c2fc971a9b733acedf920f3e8c018e94
Aoew1518 <1518703146@qq.com>
2023-12-23 17:03:55 +0800

    vue

--- vue/questionnaire/src/views/item/item.vue
+++ vue/questionnaire/src/views/item/item.vue
@@ -1,13 +1,23 @@
 <template>
     <div>
-        item
+        <ItemContainerVue parentCompoent="item"/>
     </div>
 </template>
 
 <script>
+import ItemContainerVue from '@/components/ItemContainer.vue';
+import { mapActions } from 'vuex';
     export default {
-        
-    }
+        components: {
+            ItemContainerVue
+        },
+        created() { //生命周期函数，在页面加载的时候自动会触发
+            this.getQuestionListAction()
+        },
+        methods: {
+            ...mapActions(['getQuestionListAction']),//相当于定义了getQuestionListAction()
+        }
+    }   
 </script>
 
 <style lang="less" scoped>
