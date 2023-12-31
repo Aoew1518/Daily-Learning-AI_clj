90f8a19e1f4ee09e45c1432ad5c4ce4d547e331a
Aoew1518 <1518703146@qq.com>
2023-12-31 23:05:20 +0800

    力扣

--- leetcode/树/model.js
+++ leetcode/树/model.js
@@ -4,3 +4,14 @@ function TreeNode(val) {
     this.left = this.right = null;
 }
 
+function preOrder(root) {
+    if (!root) return [];
+    let res = [];
+
+    res.push(root.val);
+    let resL = preOrder(root.left);
+    let resR = preOrder(root.right);
+
+    return res.concat(resL,resR);
+    // return [...res, ...resL, ...resR]
+}
