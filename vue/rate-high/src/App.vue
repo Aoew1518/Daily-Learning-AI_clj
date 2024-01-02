34d84e3c43bf2be55a0a026a8907f70a6e0552ac
Aoew1518 <1518703146@qq.com>
2024-01-03 00:48:09 +0800

    vue

---
+++ vue/rate-high/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Rate from './components/Rate.vue'
+import { ref } from 'vue';
+const score = ref(3);
+//  vue 开发思想是  数据与页面状态同步
+const updated = (num) => {
+  score.value = num;
+}
+setTimeout(() => {
+  score.value = 4;
+}, 2000);
+</script>
+
+<template>
+  <div>
+    <Rate :value="score" theme="red" @update-rate="updated" />
+    <!-- <Rate :value="score" /> -->
+  </div>
+</template>
+
+<style></style>
---
+++ vue/rate-high/src/components/Rate.vue
@@ -0,0 +1,72 @@
+<template>
+    <!-- 样式文本 key:val; -->
+   <div :style="fontStyle">
+    <div class="rate">
+        <span v-for="num in 5" :key="num" @mouseover="mouseOver(num)">☆</span>
+        <span class="hollow" :style="fontWidth">
+            <span v-for="num in 5" :key="num" >★</span>
+        </span>
+    </div>
+   </div>
+</template>
+
+<script setup>
+// 交接一下
+import { ref, defineProps, computed } from 'vue'
+// 自已的分值响应式变量
+
+// 主题配置
+const themeObj = {
+    black: '#000',
+    orange: '#fa5413',
+    green: '#73d13d',
+    blue: '#40a9ff',
+    red: '#f5222d'
+}
+const props = defineProps({
+    value: Number,
+    theme: {
+            type: String,
+            default: 'orange',
+    }
+})
+
+let width = ref(props.value)
+
+let rate = computed(() => "★★★★★☆☆☆☆☆".slice(5-props.value,
+ 10 - props.value))
+
+let fontStyle = computed(() => `color:${themeObj[props.theme]}`)
+let fontWidth = computed(() => `width:${width.value}em`)
+    // 1. 定义一个 props
+    // 2. 定义一个默认值
+    // 3. 定义一个类型
+    // 4. 定义一个是否必填
+    // 5. 定义一个是否是只读
+const mouseOver = (i) => {
+    width.value = i
+}
+</script>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+}
+.rate {
+    position: relative;
+    display: inline-block;
+    font-size: 0px;
+}
+.rate span {
+    font-size: 16px;
+}
+.rate > span.hollow {
+    position: absolute;
+    display: inline-block;
+    top: 0;
+    left: 0;
+    width: 0;
+    overflow: hidden;
+}
+</style>
