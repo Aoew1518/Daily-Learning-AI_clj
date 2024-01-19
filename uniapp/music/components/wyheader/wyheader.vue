9516993e7f27dba320e8b75f1a6806d18e41c964
Aoew1518 <1518703146@qq.com>
2024-01-19 17:24:07 +0800

    uniapp

---
+++ uniapp/music/components/wyheader/wyheader.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="header">
+		<uni-icons type="bars" size="24"></uni-icons>
+		
+		<view class="content">
+			<!-- 添加一个插槽 -->
+			<slot name="content"></slot>
+		</view>
+		
+		<uni-icons type="mic" size="24"></uni-icons>
+		<!-- <uni-icons type="plus-filled" size="24"></uni-icons> -->
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"wyheader",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.header{
+	height: 100rpx;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0 15rpx;
+}
+</style>
