86617f462a3b3d6dee02555c53e186162d5ffc9c
Aoew1518 <1518703146@qq.com>
2024-01-20 19:05:23 +0800

    uniapp

--- uniapp/music/api/index.js
+++ uniapp/music/api/index.js
@@ -8,3 +8,17 @@ export const apiGetBanner = (data) => {
 		data: data
 	})
 }
+
+export const apiGetBall = () => {
+		return uni.request({
+		url: baseUrl + '/homepage/dragon/ball',
+		method: 'GET',
+	})
+}
+// 推荐歌单
+export const apiGetRecommendList = () => {
+		return uni.request({
+		url: baseUrl + '/recommend/resource',
+		method: 'GET',
+	})
+}
--- uniapp/music/pages/index/index.vue
+++ uniapp/music/pages/index/index.vue
@@ -14,7 +14,7 @@
 
 		<!-- banner -->
 		<view class="banner">
-			<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" indicator-color="#eee" indicator-active-color="#d81e06" circular>
+			<swiper :indicator-dots="true" :autoplay="false" :interval="3000" :duration="1000" indicator-color="#eee" indicator-active-color="#d81e06" circular>
 				<swiper-item v-for="item in state.banners" :key="item.encodeId">
 					<view class="swiper-item">
 						<image :src="item.pic" mode=""></image>
@@ -23,36 +23,66 @@
 			</swiper>
 		</view>
 		
+		<!-- balls -->
+		<view class="balls">
+			<view class="ball-item" v-for="item in state.balls" :key="item.id">
+				<view class="icon">
+					<image :src="item.iconUrl" mode="aspectFill"></image>
+				</view>
+				<text>{{item.name}}</text>
+			</view>
+		</view>
+		
+		<!-- 专属推荐 -->
+		<songList :list="state.recommendList"/>
+		
 		
 	</view>
 </template>
 
 <script setup>
-import { apiGetBanner } from '@/api/index.js'
+import { apiGetBanner, apiGetBall, apiGetRecommendList } from '@/api/index.js'
 import { onLoad } from '@dcloudio/uni-app'
 import { reactive } from 'vue';
 
 const state = reactive({
-	banners: []
+	banners: [],
+	balls: [],
+	recommendList: [] 
 })
 
 
 onLoad(() => {
 	getBanner()
+	getBall()
+	getRecommendList()
 })
 
+// 获取banner图
 const getBanner = () => {
 	apiGetBanner({type: 2}).then(res => {
-		console.log(res.data.banners);
+		// console.log(res.data.banners);
 		state.banners = res.data.banners
 	})
+}
+// 获取入口列表
+const getBall = async() => {
+	const { data: { data: balls } } = await apiGetBall()
+	// console.log(balls);
+	state.balls = balls
+}
+// 推荐歌单
+const getRecommendList = async() => {
+	const { data: { recommend: recommend }} = await apiGetRecommendList()
+	console.log(recommend);
+	state.recommendList = recommend
 }
 
 </script>
 
 <style lang="scss" scoped>
 .index {
-	padding: 0 15rpx;
+	padding: 0 20rpx;
 	.search {
 		width: 500rpx;
 		height: 60rpx;
@@ -67,6 +97,7 @@ const getBanner = () => {
 	}
 	.banner{
 		.swiper-item{
+			width: 100%;
 			height: 100%;
 			border-radius: 10px;
 			overflow: hidden;
@@ -76,5 +107,28 @@ const getBanner = () => {
 			}
 		}
 	}
+	.balls{
+		display: flex;
+		overflow-x: scroll;
+		margin: 30rpx 0;
+		.ball-item{
+			flex: 0 0 20%;
+			font-size: 20rpx;
+			text-align: center;
+			.icon{
+				width: 70rpx;
+				height: 70rpx;
+				margin: 0 auto;
+				margin-bottom: 14rpx;
+				background-color: $uni-primary-color;
+				border-radius: 50%;
+				image{
+					width: 100%;
+					height: 100%;
+				}
+			}
+		}
+	}
+	
 }
 </style>
