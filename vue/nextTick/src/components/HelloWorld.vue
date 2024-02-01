884d8ae6f11c6b6d667f13ec55f3a167f028cc71
Aoew1518 <1518703146@qq.com>
2024-02-01 22:05:09 +0800

    vue

---
+++ vue/nextTick/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p ref="refp">消息：{{message}}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('初始消息')
+
+const refp = ref(null)
+
+setTimeout(()=>{
+  console.log(refp.value)
+},1000)
+</script>
+
+<style lang="css" scoped>
+
+</style>
