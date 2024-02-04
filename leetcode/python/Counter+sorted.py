f3ccab4c26ad934c53d31cf6deaee6d9338ebe91
Aoew1518 <1518703146@qq.com>
2024-02-04 21:10:39 +0800

    力扣

--- leetcode/python/Counter+sorted.py
+++ leetcode/python/Counter+sorted.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 nums = [0, 1, 0, 1, 0, 1, 99]
 n = len(nums)
-# res = collections.defaultdict(int)
+res = collections.defaultdict(int)
 counter = Counter(nums)
 sorted_list = sorted(counter.items(), key=lambda x: x[1], reverse=False)
 print(sorted_list[0][0])
