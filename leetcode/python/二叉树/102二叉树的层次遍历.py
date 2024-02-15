8ed5a126384296a15b510a90d3353624551a3a04
Aoew1518 <1518703146@qq.com>
2024-02-15 20:22:48 +0800

    leetcode

---
+++ leetcode/python/二叉树/102二叉树的层次遍历.py
@@ -0,0 +1,31 @@
+# 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+
+import collections
+from typing import List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if root == None: 
+            return []
+        res = []
+        queue = collections.deque()
+        queue.append(root)
+        while queue:
+            tmp = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                tmp.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(tmp)
+        return res
+
