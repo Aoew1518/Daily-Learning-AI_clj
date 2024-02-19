fa32e4f78ae42f4cd1357248a07dbcaa6f0bbf83
Aoew1518 <1518703146@qq.com>
2024-02-19 19:34:41 +0800

    vue

---
+++ vue/mini-vue/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <p>{{state.count}}</p>
+    <p>{{num}}</p>
+    <button @click="() => state.count++">add</button>
+  </div>
+</template>
+
+<script setup>
+import { computed, reactive, watch, effect } from 'vue'
+
+const state = reactive({
+  count: 1,
+  msg: 2,
+  abc: 123
+})
+
+const num = computed(() => {
+  return state.count * 10
+})
+
+watch(
+  () => state.count, 
+  (newVal, oldVal) => {
+    console.log(newVal, oldVal);
+  }
+)
+
+effect(
+  () => { // 类似于生命周期，一开始就会执行
+    console.log('hello world');
+  }, 
+  {lazy: true}
+)
+
+
+// const state2 = reactive(state)
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
