aba46cf475a52dd301aa5cceb77dc4d781d553bf
Aoew1518 <1518703146@qq.com>
2024-03-19 21:34:56 +0800

    notebook-后端

--- node/note-book/server/app.js
+++ node/note-book/server/app.js
@@ -1,16 +1,31 @@
-const Koa = require('koa')
-const bodyParse = require('koa-bodyparser')// 解析post请求的参数
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');  // 解析post请求的参数
+const cors = require('koa2-cors'); // 处理跨域
 const app = new Koa()
-const cors = require('koa2-cors')
+
 
 const user = require('./routes/user.js')
 
-app.use(bodyParse())
-app.use(cors())// 处理跨域
 
-console.log(user.allowedMethods());
+app.use(bodyParser())
+app.use(cors())  
+
+
+
+// const main = (ctx) => {
+//   if () {
+
+//   } else if () {
+
+//   } 
+//   ...
+// }
+// app.use(main)
+
 app.use(user.routes(), user.allowedMethods())
 
 app.listen(3000, () => {
-    console.log('listening on port 3000');
+  console.log('listening on port 3000');
 })
+
+
--- node/note-book/server/routes/user.js
+++ node/note-book/server/routes/user.js
@@ -1,9 +1,96 @@
 const Router = require('@koa/router')
 const router = new Router()
-// 定义接口地址
-router.post('/user/login', (ctx) => {
-    console.log(132123123);
-    console.log(ctx.request.body);
+const { userLogin, userFind, userInsert } = require('../controllers/mysqlControl.js')
+
+router.prefix('/user')
+
+// 定义接口
+router.post('/login', async(ctx) => {
+  // console.log(ctx.request.body); // 获取到了前端传递的参数
+  const { username, password } = ctx.request.body
+  
+  try {
+    const result = await userLogin(username, password)
+    // console.log(result);
+    if (result.length) {  // 查到了
+      let data = {
+        id: result[0].id,
+        nickname: result[0].nickname,
+        username: result[0].username
+      }
+
+      ctx.body = {
+        code: '8000',
+        data: data,
+        msg: '登录成功'
+      }
+    } else {
+      ctx.body = {
+        code: '8004',
+        data: 'error',
+        msg: '账号或密码错误'
+      }
+    }
+  } catch (err) {
+    ctx.body = {
+      code: '8005',
+      data: err,
+      msg: '服务器异常'
+    }
+  }
+
 })
+
+// 注册接口
+router.post('/register', async(ctx) => {
+  console.log(ctx.request.body); // 获取到了前端传递的参数
+  const { nickname, username, password } = ctx.request.body
+  // 判断参数是否为空
+  if( !nickname || !username || !password ){
+    ctx.body = {
+      code: '8001',
+      data: 'error',
+      msg: '账号密码或不能为空'
+    }
+    return
+  }
+
+  try{
+    // 判断账号是否存在
+    const findRes = await userFind(username)
+    console.log(findRes);
+    if (findRes.length) {  // 有值说明账号已存在
+      ctx.body = {
+        code: '8003',
+        data: 'error',
+        msg: '该账号已存在'
+        }
+      } 
+
+    // 添加信息到数据库
+    const result = await userInsert( nickname, username, password )
+    // console.log(result);
+    if (result.affectedRows === 1) {  // 插入成功
+      ctx.body = {
+        code: '8000',
+        data: 'success',
+        msg: '注册成功'
+      }
+    } else {
+      ctx.body = {
+        code: '8004',
+        data: 'failed',
+        msg: '注册失败'
+      }
+    }
+  }catch(error){
+    ctx.body = {
+      code: '8005',
+      data: error,
+      msg: '服务器异常'
+    }
+  }
+})
+
 
 module.exports = router
